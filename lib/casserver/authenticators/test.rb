2f8dce6f90b5ccb6a21249996b6e22da4657f820
matt.zukowski <matt.zukowski@fffcb96a-a727-0410-ad3e-7b35c796b8d7>
2007-06-05 21:13:10 +0000

    added some comments describing the test authenticator
    git-svn-id: https://rubycas-server.googlecode.com/svn/trunk@108 fffcb96a-a727-0410-ad3e-7b35c796b8d7

--- lib/casserver/authenticators/test.rb
+++ lib/casserver/authenticators/test.rb
@@ -1,5 +1,9 @@
 require 'casserver/authenticators/base'
 
+# Dummy authenticator used for testing. 
+# Accepts "testuser" for username and "testpassword" for password; otherwise authentication fails.
+# Raises an AuthenticationError when username is "do_error" (this is useful to test the Exception
+# handling functionality).
 class CASServer::Authenticators::Test < CASServer::Authenticators::Base
   def validate(credentials)
     read_standard_credentials(credentials)
