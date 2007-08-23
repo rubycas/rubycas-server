4078a059e06c67e12d989b16aa5d85f9f4b2e61f
jzylks <jzylks@fffcb96a-a727-0410-ad3e-7b35c796b8d7>
2007-08-23 02:42:59 +0000

    Added sample custom views file, may need to be more fleshed out
    git-svn-id: https://rubycas-server.googlecode.com/svn/trunk@168 fffcb96a-a727-0410-ad3e-7b35c796b8d7

---
+++ custom_views.example.rb
@@ -0,0 +1,11 @@
+# Custom views file; add methods to the module definition below
+
+module CASServer::Views
+
+  # Override views here, for example, a custom login form:
+  def login_form
+    # Add your custom login form here, using Markaby
+    # See the original views.rb file at lib/casserver/views.rb for method names and usage
+  end
+
+end
