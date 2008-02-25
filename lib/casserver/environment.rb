d918973bc90991331762415e4dd15b12f3eb3c4a
matt.zukowski <matt.zukowski@fffcb96a-a727-0410-ad3e-7b35c796b8d7>
2008-02-25 22:10:08 +0000

    better check for picnic load errors
    git-svn-id: https://rubycas-server.googlecode.com/svn/trunk@223 fffcb96a-a727-0410-ad3e-7b35c796b8d7

--- lib/casserver/environment.rb
+++ lib/casserver/environment.rb
@@ -1,12 +1,15 @@
 $: << File.dirname(File.expand_path(__FILE__))
 
-# Try to load local version of Picnic if possible...
+# Try to load local version of Picnic if possible (for development purposes)
 $: << File.dirname(File.expand_path(__FILE__))+"/../../../picnic/lib"
 $: << File.dirname(File.expand_path(__FILE__))+"/../../vendor/picnic/lib"
 
 begin
   require 'picnic'
-rescue LoadError
+rescue LoadError => e
+  # make sure that the LoadError was about picnic and not something else
+  raise e unless e.to_s =~ /picnic/
+  
   require 'rubygems'
   
   # make things backwards-compatible for rubygems < 0.9.0
