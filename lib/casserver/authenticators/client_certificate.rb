c0334a48571d1ed38a5824889afd7ac7a15c348d
matt.zukowski <matt.zukowski@fffcb96a-a727-0410-ad3e-7b35c796b8d7>
2008-05-26 18:06:28 +0000

    added starter code for Client Certificate-based authentication (see http://groups.google.com/group/rubycas-server/browse_thread/thread/6beda6521ba3ae8e)
    git-svn-id: https://rubycas-server.googlecode.com/svn/trunk@297 fffcb96a-a727-0410-ad3e-7b35c796b8d7

---
+++ lib/casserver/authenticators/client_certificate.rb
@@ -0,0 +1,46 @@
+require 'casserver/authenticators/base'
+
+# NOT YET IMPLEMENTED
+#
+# This authenticator will authenticate the user based on a client SSL certificate.
+#
+# You will probably want to use this along with another authenticator, chaining
+# it so that if the client does not provide a certificate, the server can
+# fall back to some other authentication mechanism.
+#
+# Here's an example of how to use two chained authenticators in the config.yml
+# file. The server will first use the ClientCertificate authenticator, and 
+# only fall back to the SQL authenticator of the first one fails:
+#
+# authenticator:
+#  -
+#    class: CASServer::Authenticators::ClientCertificate
+#  -
+#    class: CASServer::Authenticators::SQL
+#    database:
+#      adapter: mysql
+#      database: some_database_with_users_table
+#      user: root
+#      password:
+#      server: localhost
+#    user_table: user
+#    username_column: username
+#    password_column: password
+#
+class CASServer::Authenticators::ClientCertificate < CASServer::Authenticators::Base
+  def validate(credentials)
+    read_standard_credentials(credentials)
+    
+    @client_cert = credentials[:request]['SSL_CLIENT_CERT']
+    
+    # note that I haven't actually tested to see if SSL_CLIENT_CERT gets
+    # filled with data when a client cert is provided, but this should be
+    # the case at least in theory :)
+    
+    return false if @client_cert.blank?
+    
+    # IMPLEMENT SSL CERTIFICATE VALIDATION CODE HERE
+    
+    return true # if SSL certificate is valid, false otherwise
+  end
+end
