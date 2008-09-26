ee1d3a6213a81a895d2697b3ea3c5699dc31580d
matt.zukowski <matt.zukowski@fffcb96a-a727-0410-ad3e-7b35c796b8d7>
2008-09-26 16:47:52 +0000

    better binding when configured to bind without preauthentication [lordmortis]
    git-svn-id: https://rubycas-server.googlecode.com/svn/trunk@322 fffcb96a-a727-0410-ad3e-7b35c796b8d7

--- lib/casserver/authenticators/ldap.rb
+++ lib/casserver/authenticators/ldap.rb
@@ -28,22 +28,26 @@ class CASServer::Authenticators::LDAP < CASServer::Authenticators::Base
     return false if @password.blank?
    
     raise CASServer::AuthenticatorError, "Cannot validate credentials because the authenticator hasn't yet been configured" unless @options
-    raise CASServer::AuthenticatorError, "Invalid authenticator configuration!" unless @options[:ldap]
-    raise CASServer::AuthenticatorError, "You must specify an ldap server in the configuration!" unless @options[:ldap][:server]
-    
+    raise CASServer::AuthenticatorError, "Invalid LDAP authenticator configuration!" unless @options[:ldap]
+    raise CASServer::AuthenticatorError, "You must specify a server host in the LDAP configuration!" unless @options[:ldap][:host] || @options[:ldap][:server]
+        
     raise CASServer::AuthenticatorError, "The username '#{@username}' contains invalid characters." if (@username =~ /[*\(\)\0\/]/)
     
     preprocess_username
     
     @ldap = Net::LDAP.new
-    @ldap.host = @options[:ldap][:server]
+    
+    
+    @options[:ldap][:host] ||= @options[:ldap][:server]
+    @ldap.host = @options[:ldap][:host]
     @ldap.port = @options[:ldap][:port] if @options[:ldap][:port]
+    @ldap.encryption(@options[:ldap][:encryption]) if @options[:ldap][:encryption]
     
     begin
       if @options[:ldap][:auth_user]
-        bind_with_preauthentication
+        bind_by_username_with_preauthentication
       else
-        bind_directly
+        bind_by_username
       end
     rescue Net::LDAP::LdapError => e
       raise CASServer::AuthenticatorError,
@@ -57,23 +61,36 @@ class CASServer::Authenticators::LDAP < CASServer::Authenticators::Base
     end
   
   private
+    # Add prefix to username, if :username_prefix was specified in the :ldap config.
     def preprocess_username
-      # add prefix to username, if prefix was specified in the config
       @username = @options[:ldap][:username_prefix] + @username if @options[:ldap][:username_prefix]
     end
-    
-    def bind_with_preauthentication
-      # If an auth_user is specified, we will connect ("pre-authenticate") to the
-      # LDAP server using the authenticator account, and then attempt to bind as the
-      # user who is actually trying to authenticate. Note that you need to set up 
-      # the special authenticator account first. Also, auth_user must be the authenticator
-      # user's full CN, which is probably not the same as their username.
-      #
-      # This pre-authentication process is necessary because binding can only be done
-      # using the CN, so having just the username is not enough. We connect as auth_user, 
-      # and then try to find the target user's CN based on the given username. Then we bind
-      # as the target user to validate their credentials.
+
+    # Attempt to bind with the LDAP server using the username and password entered by
+    # the user. If a :filter was specified in the :ldap config, the filter will be
+    # added to the LDAP query for the username.
+    def bind_by_username
+      username_attribute = options[:ldap][:username_attribute] || default_username_attribute
       
+      filter = Net::LDAP::Filter.eq(username_attribute, @username)
+      unless @options[:ldap][:filter].blank?
+        filter &= Net::LDAP::Filter.construct(@options[:ldap][:filter])
+      end
+      
+      @ldap.bind_as(:base => @options[:ldap][:base], :password => @password, :filter => filter)
+    end
+
+    # If an auth_user is specified, we will connect ("pre-authenticate") with the
+    # LDAP server using the authenticator account, and then attempt to bind as the
+    # user who is actually trying to authenticate. Note that you need to set up 
+    # the special authenticator account first. Also, auth_user must be the authenticator
+    # user's full CN, which is probably not the same as their username.
+    #
+    # This pre-authentication process is necessary because binding can only be done
+    # using the CN, so having just the username is not enough. We connect as auth_user, 
+    # and then try to find the target user's CN based on the given username. Then we bind
+    # as the target user to validate their credentials.
+    def bind_by_username_with_preauthentication
       raise CASServer::AuthenticatorError, "A password must be specified in the configuration for the authenticator user!" unless 
         @options[:ldap][:auth_password]
       
@@ -81,8 +98,7 @@ class CASServer::Authenticators::LDAP < CASServer::Authenticators::Base
       
       username_attribute = options[:ldap][:username_attribute] || default_username_attribute
       
-      filter = Net::LDAP::Filter.construct(@options[:ldap][:filter]) if 
-        @options[:ldap][:filter] && !@options[:ldap][:filter].blank?
+      filter = Net::LDAP::Filter.construct(@options[:ldap][:filter]) unless @options[:ldap][:filter].blank?
       username_filter = Net::LDAP::Filter.eq(username_attribute, @username)
       if filter
         filter &= username_filter
@@ -91,18 +107,5 @@ class CASServer::Authenticators::LDAP < CASServer::Authenticators::Base
       end
       
       @ldap.bind_as(:base => @options[:ldap][:base], :password => @password, :filter => filter)
-    end
-    
-    def bind_directly
-      # When no auth_user is specified, we will try to connect directly as the user
-      # who is trying to authenticate. Note that for this to work, the username must
-      # be equivalent to the user's CN, and this is often not the case (for example,
-      # in Active Directory, the username is the 'sAMAccountName' attribute, while the
-      # user's CN is generally their full name.)
-      
-      cn = @username
-      
-      @ldap.authenticate(cn, @password)
-      @ldap.bind
     end
 end
