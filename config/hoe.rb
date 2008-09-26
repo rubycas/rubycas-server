e3bf906d24419ee5ad520926907991a88bc41178
matt.zukowski <matt.zukowski@fffcb96a-a727-0410-ad3e-7b35c796b8d7>
2008-09-26 15:42:41 +0000

    updated newgem; now requires at least activerecord 2.0.2 and activesupport 2.0.2
    git-svn-id: https://rubycas-server.googlecode.com/svn/trunk@320 fffcb96a-a727-0410-ad3e-7b35c796b8d7

---
+++ config/hoe.rb
@@ -0,0 +1,76 @@
+require 'rubycas-server/version'
+
+AUTHOR = 'Matt Zukowski'  # can also be an array of Authors
+EMAIL = "matt@zukowski.ca"
+DESCRIPTION = "Provides single sign-on authentication for web applications using the CAS protocol."
+GEM_NAME = 'rubycas-server' # what ppl will type to install your gem
+RUBYFORGE_PROJECT = 'rubycas-server' # The unix name for your project
+HOMEPATH = "http://#{RUBYFORGE_PROJECT}.rubyforge.org"
+DOWNLOAD_PATH = "http://rubyforge.org/projects/#{RUBYFORGE_PROJECT}"
+EXTRA_DEPENDENCIES = [
+  ['activesupport', '>= 2.0.2'],
+  ['activerecord', '>= 2.0.2'],
+  ['picnic', '>= 0.6.5']
+]    # An array of rubygem dependencies [name, version]
+
+@config_file = "~/.rubyforge/user-config.yml"
+@config = nil
+RUBYFORGE_USERNAME = "unknown"
+def rubyforge_username
+  unless @config
+    begin
+      @config = YAML.load(File.read(File.expand_path(@config_file)))
+    rescue
+      puts <<-EOS
+ERROR: No rubyforge config file found: #{@config_file}
+Run 'rubyforge setup' to prepare your env for access to Rubyforge
+ - See http://newgem.rubyforge.org/rubyforge.html for more details
+      EOS
+      exit
+    end
+  end
+  RUBYFORGE_USERNAME.replace @config["username"]
+end
+
+ENV['NODOT'] = '1'
+
+#REV = nil
+# UNCOMMENT IF REQUIRED:
+REV = YAML.load(`svn info`)['Revision']
+VERS = CASServer::VERSION::STRING + (REV ? ".#{REV}" : "")
+RDOC_OPTS = ['--quiet', '--title', 'rubycas-server documentation',
+    "--opname", "index.html",
+    "--line-numbers",
+    "--main", "README",
+    "--inline-source"]
+
+class Hoe
+  def extra_deps
+    @extra_deps.reject! { |x| Array(x).first == 'hoe' }
+    @extra_deps
+  end
+end
+
+# Generate all the Rake tasks
+# Run 'rake -T' to see list of generated tasks (from gem root directory)
+$hoe = Hoe.new(GEM_NAME, VERS) do |p|
+  p.developer(AUTHOR, EMAIL)
+  p.description = DESCRIPTION
+  p.summary = DESCRIPTION
+  p.url = HOMEPATH
+  p.rubyforge_name = RUBYFORGE_PROJECT if RUBYFORGE_PROJECT
+  p.test_globs = ["test/**/test_*.rb"]
+  p.clean_globs |= ['**/.*.sw?', '*.gem', '.config', '**/.DS_Store']  #An array of file patterns to delete on clean.
+
+  # == Optional
+  p.changes = p.paragraphs_of("History.txt", 0..1).join("\n\n")
+  #p.extra_deps = EXTRA_DEPENDENCIES
+
+    #p.spec_extras = {}    # A hash of extra values to set in the gemspec.
+  end
+
+CHANGES = $hoe.paragraphs_of('History.txt', 0..1).join("\\n\\n")
+PATH    = (RUBYFORGE_PROJECT == GEM_NAME) ? RUBYFORGE_PROJECT : "#{RUBYFORGE_PROJECT}"
+$hoe.remote_rdoc_dir = File.join(PATH.gsub(/^#{RUBYFORGE_PROJECT}\/?/,''), 'rdoc')
+$hoe.rsync_args = '-av --delete --ignore-errors'
+$hoe.spec.post_install_message = File.open(File.dirname(__FILE__) + "/../PostInstall.txt").read rescue ""
---
+++ lib/casserver/authenticators/open_id.rb
@@ -0,0 +1,22 @@
+require 'casserver/authenticators/base'
+
+require 'openid'
+require 'openid/extensions/sreg'
+require 'openid/extensions/pape'
+require 'openid/store/memory'
+
+
+# CURRENTLY UNIMPLEMENTED
+# This is just starter code.
+class CASServer::Authenticators::OpenID < CASServer::Authenticators::Base
+
+  def validate(credentials)
+    raise NotImplementedError, "The OpenID authenticator is not yet implemented. "+
+      "See http://code.google.com/p/rubycas-server/issues/detail?id=36 if you are interested in helping this along."
+    
+    read_standard_credentials(credentials)
+    
+    store = OpenID::Store::Memory.new
+    consumer = OpenID::Consumer.new({}, store)
+  end
+end
