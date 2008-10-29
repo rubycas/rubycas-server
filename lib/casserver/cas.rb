aa9e65c6f8236721ebce40865494ca9c1a72d2fc
matt.zukowski <matt.zukowski@fffcb96a-a727-0410-ad3e-7b35c796b8d7>
2008-10-29 20:38:00 +0000

    single-sign-out request implementation changed to comply with JA-SIG CAS Server 3.3's implementation
    git-svn-id: https://rubycas-server.googlecode.com/svn/trunk@368 fffcb96a-a727-0410-ad3e-7b35c796b8d7

--- lib/casserver/cas.rb
+++ lib/casserver/cas.rb
@@ -234,28 +234,31 @@ module CASServer::CAS
   # See http://www.ja-sig.org/wiki/display/CASUM/Single+Sign+Out
   def send_logout_notification_for_service_ticket(st)
     uri = URI.parse(st.service)
-    http = Net::HTTP.new(uri.host,uri.port)
+    http = Net::HTTP.new(uri.host, uri.port)
     #http.use_ssl = true if uri.scheme = 'https'
     
-    http.start do |conn|
-      path = uri.path
-      path = '/' if path.empty?
-      
-      time = Time.now
-      rand = CASServer::Utils.random_string
-      
-      data = %{<samlp:LogoutRequest ID="#{rand}" Version="2.0" IssueInstant="#{time.rfc2822}">
+    time = Time.now
+    rand = CASServer::Utils.random_string
+    
+    path = uri.path
+    path = '/' if path.empty?
+    
+    req = Net::HTTP::Post.new(path)
+    req.set_form_data(
+      'logoutRequest' => %{<samlp:LogoutRequest ID="#{rand}" Version="2.0" IssueInstant="#{time.rfc2822}">
 <saml:NameID></saml:NameID>
 <samlp:SessionIndex>#{st.ticket}</samlp:SessionIndex>
 </samlp:LogoutRequest>}
-      
-      response = conn.request_post(path, data)
-      
-      if response.code.to_i == 200
+    )
+    
+    http.start do |conn|
+      response = conn.request(req)
+            
+      if response.kind_of? Net::HTTPSuccess
         $LOG.info "Logout notification successfully posted to #{st.service.inspect}."
         return true
       else
-        $LOG.error "Service #{st.service.inspect} responed to logout notification with code '#{response.code}'."
+        $LOG.error "Service #{st.service.inspect} responed to logout notification with code '#{response.code}'!"
         return false
       end
     end
