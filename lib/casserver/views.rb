95689facd8dc8994468967534a6702a9b1c56830
Antono Vasiljev <antono.vasiljev@gmail.com>
2008-11-04 17:51:25 +0200

    updated russian translation and pot file

--- lib/casserver/views.rb
+++ lib/casserver/views.rb
@@ -64,7 +64,7 @@ module CASServer::Views
   # Just the login form.
   def login_form
     form(:method => "post", :action => @form_action || '/login', :id => "login-form",
-        :onsubmit => "submitbutton = document.getElementById('login-submit'); submitbutton.value='#{_("Please wait...")}'; submitbutton.disabled=true; return true;") do
+        :onsubmit => "submitbutton = document.getElementById('login-submit'); submitbutton.value='#{ _("Please wait...") }'; submitbutton.disabled=true; return true;") do
       table(:id => "form-layout") do
         tr do
           td(:id => "username-label-container") do
