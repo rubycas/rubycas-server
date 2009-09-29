eb3108a8b9ca2ce46959669411e44437bd028c5e
Lin Jen-Shin <godfat@godfat.org>
2009-09-29 15:27:37 +0800

    [localization.rb] case-insensitive match for lang chooser.

--- lib/casserver/localization.rb
+++ lib/casserver/localization.rb
@@ -62,7 +62,8 @@ module CASServer
 
     chosen_lang = nil
     langs.each do |l|
-      a = available.find{|a| a == l || a =~ Regexp.new("#{l}-\w*")}
+      a = available.find{ |a| a =~ Regexp.new("\\A#{l}\\Z", 'i') ||
+                              a =~ Regexp.new("#{l}-\w*",   'i')    }
       if a
         chosen_lang = a
         break
