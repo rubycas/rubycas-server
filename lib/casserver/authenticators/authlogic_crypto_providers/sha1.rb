90a2fa1789c3507932bfc405fa5015eda84643ff
Damien Le Berrigaud <damien.leberrigaud@blake.com.au>
2010-09-21 12:34:48 +0800

    Updated Authlogic support:  - support for Encoder Options in configuration.  - support for *basic* Sha1 encryption, with options: stretches=1, and digest_format specified.

--- lib/casserver/authenticators/authlogic_crypto_providers/sha1.rb
+++ lib/casserver/authenticators/authlogic_crypto_providers/sha1.rb
@@ -2,8 +2,9 @@ require "digest/sha1"
 
 module Authlogic
   module CryptoProviders
-    # This class was made for the users transitioning from restful_authentication. I highly discourage using this
-    # crypto provider as it inferior to your other options. Please use any other provider offered by Authlogic.
+    # This class was made for the users transitioning from restful_authentication.
+    # I highly discourage using this crypto provider as it inferior to your other options.
+    # Please use any other provider offered by Authlogic.
     class Sha1
       class << self
         def join_token
@@ -11,7 +12,24 @@ module Authlogic
         end
         attr_writer :join_token
 
-        # The number of times to loop through the encryption. This is ten because that is what restful_authentication defaults to.
+        def digest_format=(format)
+          @digest_format = format
+        end
+
+        # This is for "old style" authentication with a custom format of digest
+        def digest(tokens)
+          if @digest_format
+            @digest_format.
+              gsub('PASSWORD', tokens.first).
+              gsub('SALT', tokens.last)
+          else
+            tokens.join(join_token)
+          end
+        end
+
+        # The number of times to loop through the encryption.
+        # This is ten because that is what restful_authentication defaults to.
+
         def stretches
           @stretches ||= 10
         end
@@ -20,9 +38,15 @@ module Authlogic
         # Turns your raw password into a Sha1 hash.
         def encrypt(*tokens)
           tokens = tokens.flatten
-          digest = tokens.shift
-          stretches.times { digest = Digest::SHA1.hexdigest([digest, *tokens].join(join_token)) }
-          digest
+
+          if stretches > 1
+            hash = tokens.shift
+            stretches.times { hash = Digest::SHA1.hexdigest([hash, *tokens].join(join_token)) }
+          else
+            hash = Digest::SHA1.hexdigest( digest(tokens) )
+          end
+
+          hash
         end
 
         # Does the crypted password match the tokens? Uses the same tokens that were used to encrypt.
