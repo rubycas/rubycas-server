bca943238fde66d102c5903956fd4fefcacb7b3e
Damien Le Berrigaud <damien.leberrigaud@blake.com.au>
2010-09-22 15:30:12 +1000

    Require ruby-debug if --debugger option given to binary.

--- lib/casserver.rb
+++ lib/casserver.rb
@@ -1,2 +1,14 @@
-module CASServer
-end
+module CASServer; end
+
+$: << File.expand_path(File.dirname(__FILE__) + '/casserver')
+$: << File.expand_path(File.dirname(__FILE__) + '/../vendor/isaac_0.9.1')
+
+require 'active_record'
+require 'active_support'
+require 'sinatra/base'
+require 'haml'
+require 'logger'
+$LOG = Logger.new(STDOUT)
+
+require 'server'
+
