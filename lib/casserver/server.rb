c6f8709e8dd92982347ad4105020c5a6a584fcdf
David Yip <yipdw@member.fsf.org>
2011-08-12 12:51:41 -0500

    Set @pgt_url in /serviceValidate from the pgt parameter.

--- lib/casserver/server.rb
+++ lib/casserver/server.rb
@@ -627,6 +627,7 @@ module CASServer
 			@service = clean_service_url(params['service'])
 			@ticket = params['ticket']
 			# optional
+			@pgtUrl = params['pgtUrl']
 			@renew = params['renew']
 
 			st, @error = validate_service_ticket(@service, @ticket)
