96720c9f2347bf3ce955d6601446074a32f95233
Tyler Pickett <t.pickett66@gmail.com>
2012-10-02 11:15:27 -0500

    remove random_string tests
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- spec/utils_spec.rb
+++ spec/utils_spec.rb
@@ -5,35 +5,6 @@ module CASServer
 end
 require 'casserver/utils'
 
-describe CASServer::Utils, '#random_string(max_length = 29)' do
-  before do
-    load_server(File.dirname(__FILE__) + "/default_config.yml")
-    reset_spec_database
-  end
-  
-  context 'when max length is not passed in' do
-    it 'should return a random string of length 29' do
-      subject.random_string.length.should == 29
-    end
-  end
-
-  context 'when max length is passed in' do
-    it 'should return a random string of the desired length' do
-      subject.random_string(30).length.should == 30
-    end
-  end
-
-  it 'should include the letter r in the random string' do
-    subject.random_string.should include 'r'
-  end
-
-  it 'should return a random string' do
-    random_string = subject.random_string
-    another_random_string = subject.random_string
-    random_string.should_not == another_random_string
-  end
-end
-
 describe CASServer::Utils, '#log_controller_action(controller, params)' do
   let(:params) { {} }
   let(:params_with_password) { { 'password' => 'test' } }
