6834c623f3bd02abdc77c99fa82d08c658eefee8
Tyler Pickett <t.pickett66@gmail.com>
2012-10-02 11:15:50 -0500

    plug in new random method and issue deprecation warning
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- lib/casserver/utils.rb
+++ lib/casserver/utils.rb
@@ -4,12 +4,8 @@ require 'crypt-isaac'
 module CASServer
   module Utils
     def random_string(max_length = 29)
-      rg =  Crypt::ISAAC.new
-      max = 4294619050
-      r = "#{Time.now.to_i}r%X%X%X%X%X%X%X%X" %
-        [rg.rand(max), rg.rand(max), rg.rand(max), rg.rand(max),
-         rg.rand(max), rg.rand(max), rg.rand(max), rg.rand(max)]
-      r[0..max_length-1]
+      puts "Utils.random_string is deprecated, please use String.random! (#{caller[0]})"
+      String.random(max_length)
     end
     module_function :random_string
 
