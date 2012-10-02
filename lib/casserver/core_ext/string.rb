13a47ad3abb8df878cb55089c5bbe9c8b71326ab
Tyler Pickett <t.pickett66@gmail.com>
2012-10-02 11:15:02 -0500

    use urlsafe_base64 since tickets go in urls.
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- lib/casserver/core_ext/string.rb
+++ lib/casserver/core_ext/string.rb
@@ -8,8 +8,11 @@ module CASServer
       end
 
       module ClassMethods
+        # if we're on ruby 1.9 we'll use the built in version
+        # this will break if someone trys to use ActiveSupport 3.2+
+        # with Ruby 1.8
         def random(length = 29)
-          str = "#{Time.now.to_i}r#{SecureRandom.base64(length)}"
+          str = "#{Time.now.to_i}r#{SecureRandom.urlsafe_base64(length)}"
           str[0..(length - 1)]
         end
       end
