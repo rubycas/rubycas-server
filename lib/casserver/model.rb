8c2f180f538eda3c533edb50e69bc6760e77cdcd
Tyler Pickett <t.pickett66@gmail.com>
2012-10-03 12:03:11 -0500

    extract Consumable to separate file
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- lib/casserver/model.rb
+++ lib/casserver/model.rb
@@ -1,35 +1,8 @@
+require 'casserver/model/consumable'
 require 'active_record'
 require 'active_record/base'
 
 module CASServer::Model
-
-  module Consumable
-    def consume!
-      self.consumed = Time.now
-      self.save!
-    end
-
-    def self.included(mod)
-      mod.extend(ClassMethods)
-    end
-
-    module ClassMethods
-      def cleanup(max_lifetime, max_unconsumed_lifetime)
-        transaction do
-          conditions = ["created_on < ? OR (consumed IS NULL AND created_on < ?)",
-                          Time.now - max_lifetime,
-                          Time.now - max_unconsumed_lifetime]
-                        
-          expired_tickets_count = count(:conditions => conditions)
-
-          $LOG.debug("Destroying #{expired_tickets_count} expired #{self.name.demodulize}"+
-            "#{'s' if expired_tickets_count > 1}.") if expired_tickets_count > 0
-
-          destroy_all(conditions)
-        end
-      end
-    end
-  end
 
   class Ticket < ActiveRecord::Base
     def to_s
