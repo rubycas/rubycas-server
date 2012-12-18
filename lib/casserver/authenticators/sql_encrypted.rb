9cb099ddd5b8115f69dbbb4958f07ac3cc08e6c4
Robert Mitwicki <robert.mitwicki@fxitech.com>
2012-12-18 15:11:15 +0100

    Fix #147

--- lib/casserver/authenticators/sql_encrypted.rb
+++ lib/casserver/authenticators/sql_encrypted.rb
@@ -52,10 +52,10 @@ class CASServer::Authenticators::SQLEncrypted < CASServer::Authenticators::SQL
     username_column = @options[:username_column] || "username"
     encrypt_function = @options[:encrypt_function] || 'user.encrypted_password == Digest::SHA256.hexdigest("#{user.encryption_salt}::#{@password}")'
 
-    $LOG.debug "#{self.class}: [#{user_model}] " + "Connection pool size: #{user_model.connection_pool.instance_variable_get(:@checked_out).length}/#{user_model.connection_pool.instance_variable_get(:@connections).length}"
+    $LOG.debug "#{self.class}: [#{user_model}] " + "Connection pool size: #{user_model.connection_pool.connections.length}/#{user_model.connection_pool.instance_variable_get(:@size)}"
     results = user_model.find(:all, :conditions => ["#{username_column} = ?", @username])
     user_model.connection_pool.checkin(user_model.connection)
-    
+
     if results.size > 0
       $LOG.warn("Multiple matches found for user '#{@username}'") if results.size > 1
       user = results.first
