a7e65b7b31ee8d5e7bdc8f44e9ea888a1a7a1199
Vivek Parihar <vivek@weboniselab.com>
2013-02-04 16:22:23 +0530

    removed cannot load such file -- lib/casserver/authenticators/sql_encrypted.rb (LoadError) for SqlEncrypted authenticator

--- lib/casserver.rb
+++ lib/casserver.rb
@@ -12,7 +12,7 @@ CASServer::Authenticators.autoload :LDAP, 'casserver/authenticators/ldap.rb'
 CASServer::Authenticators.autoload :ActiveDirectoryLDAP, 'casserver/authenticators/active_directory_ldap.rb'
 CASServer::Authenticators.autoload :SQL, 'casserver/authenticators/sql.rb'
 CASServer::Authenticators.autoload :Google, 'casserver/authenticators/google.rb'
-CASServer::Authenticators.autoload :SQLEncrypted, 'lib/casserver/authenticators/sql_encrypted.rb'
+CASServer::Authenticators.autoload :SQLEncrypted, 'casserver/authenticators/sql_encrypted.rb'
 CASServer::Authenticators.autoload :ActiveResource, 'casserver/authenticators/active_resource.rb'
 
 require 'casserver/server'
